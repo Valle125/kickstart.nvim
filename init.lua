9854c9dfa18c3cbb9184470623c567147c926436
Valentin Platzgummer <valentin.platzgummer@ymail.com>
2025-04-01 20:50:12 +0200

    remove c_templates

--- init.lua
+++ init.lua
@@ -451,13 +451,6 @@ require('lazy').setup({
         }
       end, { desc = '[S]earch [A]ll Projects' })
 
-      vim.keymap.set('n', '<leader>st', function()
-        builtin.live_grep {
-          cwd = vim.fn.stdpath 'config' .. '/c_templates',
-          prompt_title = 'Live Grep C-Templates',
-        }
-      end, { desc = '[S]earch [T]emplates' })
-
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
